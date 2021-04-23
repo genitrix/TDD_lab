d4e90ad6697092ac176f84d5f1d9d4b1649a33ab
protrix <protrix@users.noreply.github.com>
2021-04-23 14:31:43 +0800

    chapter 5

--- functional_tests.py
+++ functional_tests.py
@@ -3,8 +3,10 @@ import unittest
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 # 把测试写成类，它继承unittest.TestCase；
 class NewVisitorTest(unittest.TestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -19,43 +21,54 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notice the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
-        header_next = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn("TO-Do", header_next)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("To-Do", header_text)
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        # self.assertEqual(
+        #     inputbox.get_attribute('placeholder'),
+        #     'Enter a to-do item'
+        # )
 
         # She types "Buy peacock feathers" into a text box(Edith's hobby
         # is tying fly-fishing lures)
-        inputbox.send_keys("Buy peacock feathers")
 
+        # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
+        self.fail("Finish the tests!")
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     "New to-do item did not appear in table"
+        # )
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
+        # )
 
         # There is still  text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail("Finish the tests!")
+
 
 if __name__ == "__main__":
+    unittest.main(warnings='ignore')
     # unittest.main（）方法，
     # 它启动unittest测试运行器，
     # 它将自动在文件中寻找测试类和方法并运行它们。
     # 防止了在撰写本文时爆出多余的ResourceWarning信息。
-    unittest.main(warnings = 'ignore')
-browser = webdriver.Firefox()
+    # browser = webdriver.Firefox()
 
 # Edith has heard about a cool new online to-do app. She goes
 # to check out its homepage
@@ -68,6 +81,7 @@ browser = webdriver.Firefox()
 
 # She types "Buy peacock feathers" into a text box (Edith's hobby
 # is typing fly-fishing lures)
+
 
 # when she hits enter, the page updates, and now the page lists
 # "1: Buy peacock feathers" as an item in a to-do list
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html',{
+        'new-item_text': request.POST.get('item_text',''),
+    })
     # return HttpResponse('<html><title>To-Do lists</title></html>')
