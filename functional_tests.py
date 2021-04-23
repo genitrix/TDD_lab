3abded6d3fa9570edef2b3d4972b1e6094406845
protrix <protrix@users.noreply.github.com>
2021-04-23 14:40:54 +0800

    chapter 5+

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     # 以test开头的任何方法都是测试方法
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edit has heard about a cool new online to-do app. She goes
@@ -29,6 +34,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
         # self.assertEqual(
         #     inputbox.get_attribute('placeholder'),
         #     'Enter a to-do item'
@@ -37,14 +45,14 @@ class NewVisitorTest(unittest.TestCase):
         # She types "Buy peacock feathers" into a text box(Edith's hobby
         # is tying fly-fishing lures)
 
-        # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        # self.assertIn(
+        #     '2: Use peacock feathers to make a fly',
+        #     [row.text for row in rows]
+        # )
         self.fail("Finish the tests!")
         # self.assertTrue(
         #     any(row.text == '1: Buy peacock feathers' for row in rows),
