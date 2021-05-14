2720e3124f5e32ed9517920ba4ff0a76b9d6b0c0
Matrix <matrix@MatrixdeMacBook-Pro.local>
2021-05-14 14:58:49 +0800

    chapter 6 page 20

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,9 @@ import unittest
 from selenium.webdriver.common.keys import Keys
 import time
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
 
 
 # 把测试写成类，它继承unittest.TestCase；
@@ -14,39 +17,125 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def test_can_start_a_list_for_one_user(self):
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
-    # 以test开头的任何方法都是测试方法
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edit has heard about a cool new online to-do app. She goes
-        # to check out its homepage
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    # add from demo code
+    def test_can_start_a_list_for_one_user(self):
+        # edith has heard about a cool new online to-do app.
+        # she goes to check out its homepage
         self.browser.get(self.live_server_url)
-        #self.browser.get("http://localhost:8000")
 
-        # She notice the page title and header mention to-do lists
+        # She notices the page title and header metion to-do
+        # lists
         self.assertIn("To-Do", self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn("To-Do", header_text)
+        self.assertIn('To-Do', header_text)
 
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # She types "Buy peacock feathers" into a text box
+        # (Edith's hobby is tying fly-fishing lures)
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # self.fail('Finsh the test!')
+
+        # # She notice the page title and header mention to-do lists
+        # self.assertIn("To-Do", self.browser.title)
+        # header_text = self.browser.find_element_by_tag_name('h1').text
+        # self.assertIn("To-Do", header_text)
+
+    # 以test开头的任何方法都是测试方法
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edit has heard about a cool new online to-do app. She goes
+        # 1 to check out its homepage
+        self.browser.get(self.live_server_url)
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her lists has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        # end 1
+
+        # start page 6.18
+        self.browser.quit()
+        self.browser = webdriver.Firefox
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feather', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # satisfied, they both go back to sleep
+        # end page 6.18
+
         # sleep is required
         # 刷新太快，需要sleep
         time.sleep(1)
         # get again
         # 对象会变，再获取一次
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys("Use peacock feathers to make a fly")
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(10)
+        # removed
+
+        # inputbox = self.browser.find_element_by_id('id_new_item')
+        # inputbox.send_keys("Use peacock feathers to make a fly")
+        # inputbox.send_keys(Keys.ENTER)
+
+        # time.sleep(10)
+
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
+        # removed
+
+        # self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        # self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
         # self.assertEqual(
         #     inputbox.get_attribute('placeholder'),
         #     'Enter a to-do item'
@@ -55,7 +144,6 @@ class NewVisitorTest(LiveServerTestCase):
         # She types "Buy peacock feathers" into a text box(Edith's hobby
         # is tying fly-fishing lures)
 
-
         # table = self.browser.find_element_by_id('id_list_table')
         # rows = table.find_elements_by_tag_name('tr')
         # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
@@ -63,7 +151,11 @@ class NewVisitorTest(LiveServerTestCase):
         #     '2: Use peacock feathers to make a fly',
         #     [row.text for row in rows]
         # )
-        self.fail("Finish the tests!")
+
+        # removed
+
+        # self.fail("Finish the tests!")
+
         # self.assertTrue(
         #     any(row.text == '1: Buy peacock feathers' for row in rows),
         #     "New to-do item did not appear in table"
